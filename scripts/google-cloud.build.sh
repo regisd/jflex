39c02d09b97ccd2c6e965f107e96758364dfc75e
Régis Décamps <regisd@google.com>
2020-10-27 21:44:20 +0100

    Try bazel --output_base

--- scripts/google-cloud.build.sh
+++ scripts/google-cloud.build.sh
@@ -1,5 +1,5 @@
-BAZEL='bazel --bazelrc=.gcp.bazelrc'
+BAZEL='bazel --bazelrc=.gcp.bazelrc --output_base=/tmp/_bazel'
 $BAZEL info
 $BAZEL build --config=gcp //... jflex/jflex_bin_deploy.jar
 $BAZEL test  --config=gcp //...
-find /workspace/bazel-testlogs/
+
